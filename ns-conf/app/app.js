630e600d632fdf20270c38d085a11110ae5b3175
Ice User5 <iceuser5@telerik.local>
2016-03-25 11:33:39 +0000

    App created.

---
+++ ns-conf/app/app.js
@@ -0,0 +1,7 @@
+var application = require('application');
+
+// START_CUSTOM_CODE_nativeScriptApp
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_nativeScriptApp
+application.start({ moduleName: "navigation/navigation" });
